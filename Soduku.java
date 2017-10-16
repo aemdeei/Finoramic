42cb03d94de12265e15d1133932e468f41ad95c8
Mrinmoy Das <mrinmoydas3011@gmail.com>
2017-10-17 02:02:29 +0530

    Add files via upload

---
+++ Soduku.java
@@ -0,0 +1,67 @@
+public class Solution 
+{
+	public int isValidSudoku(final List<String> a) 
+	{
+	    if(a==null)
+	        return 0;
+	    int rowsize=a.size();
+	    int columnsize=a.get(0).length();
+	    if(rowsize!=9 || columnsize!=9)
+	        return 0;
+	    HashSet<Character> b=new HashSet<>();
+	    for(int i=0;i<9;i++)//checking each row
+	    {
+	        b.clear();
+	        for(int j=0;j<9;j++)
+	        {
+	            char c=a.get(i).charAt(j);
+	            if(c=='.')
+	                continue;
+	            else
+	            {
+	                if(b.contains(c)==true)
+	                    return 0;
+	            }
+	            b.add(c);
+	        }
+	    }
+	    for(int i=0;i<9;i++)//checking each column
+	    {
+	        b.clear();
+	        for(int j=0;j<9;j++)
+	        {
+	            char c=a.get(j).charAt(i);
+	            if(c=='.')
+	                continue;
+	            else
+	            {
+	                if(b.contains(c)==true)
+	                    return 0;
+	            }
+	            b.add(c);
+	        }
+	    }
+	    for(int k=0;k<9;k++)
+	    {
+	        int start=k/3;
+	        int end=k%3;
+	        b.clear();
+	        for(int i=(3*start);i<(3*start)+3;i++)
+	        {
+	            for(int j=(3*end);j<(3*end)+3;j++)
+	            {
+	                char c=a.get(i).charAt(j);
+	                if(c=='.')
+	                    continue;
+	                else
+	                {
+	                    if(b.contains(c)==true)
+	                        return 0;
+	                }
+	                b.add(c);
+	            }
+	        }
+	    }
+	    return 1;
+	}
+}
